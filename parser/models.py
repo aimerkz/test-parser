ae5a88ded0b5346a863fe1fb736b6a71fd4328d8
amerkulov <amerkulov@aorti.ru>
2023-01-14 10:42:45 +0300

    update pars

--- parser/models.py
+++ parser/models.py
@@ -3,6 +3,8 @@ from pydantic import BaseModel, Field, root_validator
 
 class Address(BaseModel):
     value: str
+    lat: float
+    lng: float
 
 
 class Salary(BaseModel):
@@ -10,10 +12,29 @@ class Salary(BaseModel):
     to: int
 
 
+class Phone(BaseModel):
+    city: str
+    country: str
+    number: str
+
+
 class Contacts(BaseModel):
-    fullName: str
-    phone: str
     email: str
+    fullName: str = Field(alias='name')
+    phone: Phone
+
+    class Config:
+        allow_population_by_field_name = True
+
+    @root_validator(pre=True)
+    def validate_phone(cls, values):
+        phone = values['phone']
+        city = phone[1:4]
+        country = phone[0]
+        number = phone[4:]
+        number = number[0:3] + '-' + number[3:5] + '-' + number[5:]
+        values['phone'] = {'city': city, 'country': country, 'number': number}
+        return values
 
 
 class BasePars(BaseModel):
@@ -23,11 +44,13 @@ class BasePars(BaseModel):
     business_area: int = 1
     contacts: Contacts
     description: str
-    employment: str
     name: str
     salary: Salary
 
     @root_validator()
     def validate(cls, values):
+        lat = values['address'].lat
+        lng = values['address'].lat
+        values['coordinates'] = {'latitude': lat, 'longitude': lng}
         values['address'] = values['address'].value
         return values
